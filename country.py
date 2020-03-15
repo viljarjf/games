3d515021bfe783f692257a8dc2b1c47c29f47726
Viljar Femoen <viljarjf@stud.ntnu.no>
2020-03-15 14:36:44 +0100

    Added Regions
    Added regions as an enum class

--- country.py
+++ country.py
@@ -4,14 +4,14 @@ Created on Sun Mar 15 10:48:57 2020
 
 @author: Viljar Femoen
 """
-from population import *
+from population import Population
 
 class Country:
 
     def __init__(self, name, region,  population, airports, ports, borders):
         self.name = name #str
         self.region = region #region
-        self.population = Population(population) # population
+        self.population = Population(population) # int
         self.airports = airports #list of airports
         self.ports = ports # list of ports
         self.borders = borders #list of countries
---
+++ region.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 14:22:10 2020
+
+@author: Viljar Femoen
+"""
+
+from enum import Enum
+
+class Region(Enum):
+    Scandinavia = 1
+    Baltics = 2
+    C_Europe = 3
+    W_Europe = 4
+    E_Europe = 5
+    N_Africa = 6
+    W_Africa = 7
+    C_Africa = 8
+    S_Africa = 9
+    E_Africa = 10
+    Middle_East = 11
+    C_Asia = 12
+    W_Asia = 13
+    SE_Asia = 14
+    NE_Asia = 15
+    E_Asia = 16
+    N_Asia = 17
+    Oceania = 18
+    N_America = 19
+    C_America = 20
+    S_America = 21
