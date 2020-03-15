04e618df2b35ad380f0bb78b2913dedcabec9897
Viljar Femoen <viljarjf@stud.ntnu.no>
2020-03-15 17:00:02 +0100

    Created gløs.py
    Finalised the change from world to gløs, and created gløs.py

--- country.py
+++ country.py
@@ -8,13 +8,14 @@ from population import Population
 from building import Building
 from route import Route
 import random
+import weakref
 
 class Room:
 
     #name: str
     #region: Region
     #population: int
-    #routes: list of route names and amount ([[A, 5], [B, 100])
+    #routes: list of route names and percentage ([[A, 5], [B, 85], [C, 10]])
     def __init__(self, name, building,  population, routes):
         self.name = name 
         self.building = building 
@@ -22,9 +23,20 @@ class Room:
         self.routes = [] #list of routes  
 
         for room in routes:
-            self.routes.append(Route(self.name, room[0], room[1])) #appends a new route to self.routes
+            self.routes.append(Route(room[0], room[1])) #appends a new route to self.routes
 
+    def getName(self):
+        return self.name
 
     def isInfected(self):
         return self.population.isInfected()
     
+    def move(self):
+        for route in self.routes:
+            fracInf = self.population.getFractionInfected()
+            infected = route.amount*fracInf
+            route.destination.infect(int(infected*0.1))
+
+    def infect(self, amount):
+        self.population.infect(amount)
+
