845f6de11755af3b01f08d70eb9aa86f911212df
Viljar Femoen <viljar.femoen@hotmail.no>
2020-08-20 10:14:38 +0200

    Changed board from list to ndarray. Added set and move functions

--- 4d_chess/board.py
+++ 4d_chess/board.py
@@ -2,6 +2,7 @@ from tile import Tile
 from piece import Piece
 
 import copy
+import numpy
 ### Chess board
 class Board:
     
@@ -12,7 +13,7 @@ class Board:
         for n in range(dimension):
             board = [board]*dimension
         
-        self._tiles = board
+        self._tiles = numpy.array(board)
     
     def get_tile(self, pos: tuple)-> Tile:
         l = copy.deepcopy(self._tiles) 
@@ -24,6 +25,48 @@ class Board:
         for n in pos:
             if n > self._dim or n < 0:
                 raise IndexError("Tile position out of bounds")
-            l = l[n]
-        return l
 
+        return l[pos]
+    
+    def set_tile(self, pos: tuple, piece: Piece)-> bool:
+        """Set a tile to contain a new piece
+
+        Args:
+            pos (tuple): Position to update.
+            piece (Piece): The piece to put in the tile
+
+        Returns:
+            bool: Success state of the function
+        """
+        try:
+            # check if the position is valid
+            self.get_tile(pos)
+
+            self._tiles[pos] = Piece
+            return True
+        except:
+            return False
+
+    def move(self, start: tuple, end: tuple)-> bool:
+        """Move a piece from start to end. Does not check wether the piece is allowed to move there.
+
+        Args:
+            start (tuple): start position 
+            end (tuple): destination position
+
+        Returns:
+            bool: Success or failure of the move
+        """
+        p_start = self.get_tile(start).get_piece()
+        p_end = self.get_tile(end).get_piece()
+    
+        # start is empty
+        if p_start.get_color() == None:
+            return False
+
+        # same team
+        if  p_start.get_color() == p_end.get_color():
+            return False
+              
+        self.set_tile(end, p_start)
+        self.set_tile(start, Piece())
